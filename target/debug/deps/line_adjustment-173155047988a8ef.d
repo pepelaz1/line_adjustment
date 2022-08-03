18d65e5ffa7a8493be1e8f7a40ddfe59e1ef509f
pepelaz <pepelaz101@gmail.com>
2022-08-03 12:40:56 +0700

    first version

--- target/debug/deps/line_adjustment-173155047988a8ef.d
+++
@@ -1,7 +0,0 @@
-/Users/pepelaz/work/mine/rust/line_adjustment/target/debug/deps/line_adjustment-173155047988a8ef.rmeta: src/lib.rs
-
-/Users/pepelaz/work/mine/rust/line_adjustment/target/debug/deps/libline_adjustment-173155047988a8ef.rlib: src/lib.rs
-
-/Users/pepelaz/work/mine/rust/line_adjustment/target/debug/deps/line_adjustment-173155047988a8ef.d: src/lib.rs
-
-src/lib.rs:
