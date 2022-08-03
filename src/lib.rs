814c968f3d55704e0d3541efa7630ec6fee04b8a
pepelaz <pepelaz101@gmail.com>
2022-08-03 08:52:42 +0700

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+pub fn transform(input: &str, line_width: u32) -> String {
+    "".to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::transform;
+
+    #[test]
+    fn simple() {
+        let test_cases = [
+            ("", 5, ""),
+            ("test", 5, "test "),
+            ("Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua", 12,
+             "Lorem  ipsum\ndolor    sit\namet        \nconsectetur \nadipiscing  \nelit  sed do\neiusmod     \ntempor      \nincididunt  \nut labore et\ndolore magna\naliqua      ")
+        ];
+
+        for &(input, line_width, expected) in &test_cases {
+            println!("input: '{}'", input);
+            assert_eq!(transform(input, line_width), expected);
+        }
+    }
+}
