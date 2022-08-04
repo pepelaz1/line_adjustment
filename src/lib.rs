50f607047b1941f302118aef5ebbff812a89c762
pepelaz <pepelaz101@gmail.com>
2022-08-04 11:52:57 +0700

    fix remarks 1

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,7 @@ mod tests {
             ("one two three", 6, "one   \ntwo   \nthree "),
             ("one two three", 7, "one two\nthree  "),
             ("one two three", 10, "one    two\nthree     "),
-             ("one two three", 13, "one two three"),
+            ("one two three", 13, "one two three"),
             ("one two three", 14, "one  two three"),
             ("one two three", 15, "one  two  three"),
             ("one two three", 16, "one   two  three"),
@@ -26,7 +26,8 @@ mod tests {
             ("раз два три четыре", 2, "too long word"),
             ("只要功夫深 铁杵磨成针", 7, "只要功夫深  \n铁杵磨成针  "),
             ("只要功夫深 铁杵磨成针 爱美之心 人皆有之", 20, "只要功夫深   铁杵磨成针   爱美之心\n人皆有之                "),
-            ("one two three four", 6, "one   \ntwo   \nthree \nfour  "),
+            ("aaaaa bbbbb cccc dddd", 20, "aaaaa   bbbbb   cccc\ndddd                "),
+             ("one two three four", 6, "one   \ntwo   \nthree \nfour  "),
             ("Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua", 12,
                 "Lorem  ipsum\ndolor    sit\namet        \nconsectetur \nadipiscing  \nelit  sed do\neiusmod     \ntempor      \nincididunt  \nut labore et\ndolore magna\naliqua      "),
             ("Mary and Samantha arrived at the bus station early but waited until noon for the bus", 10,
@@ -42,29 +43,35 @@ mod tests {
 
 pub fn transform(input: &str, line_width: u32) -> String {
     let mut output = String::new();
-    let mut current = String::new();
     let mut chars_count: u32 = 0;
     let mut i: u32 = 0;
-    let mut words: Vec<String> = Vec::new();
-    // let mut words: Vec<&String> = Vec::new();
+    let mut words: Vec<String> = vec!["".to_string()];
+    let mut word_num: usize = 0;
+    let mut words_count: u32 = 0;
 
+    // enumerate all chars of input string, split to words, place words to vector and transter these words to output string together with spaces calculated by task conditions
     for c in input.chars() {
    
          if i == line_width + 1 {
-
-            if process_words(&mut words, &mut output, line_width, &mut chars_count) == false {
+  
+            if process_words(&mut words, &mut words_count, &mut word_num, &mut output, line_width, &mut chars_count) == false {
                 return "too long word".to_string();
             }
 
-            i = current.chars().count() as u32;
+            i = words[word_num].chars().count() as u32;
         }
 
         if c == ' ' {
-            words.push(current.clone());
-            chars_count = chars_count + current.chars().count() as u32;
-            current.clear();
+            // calculates count of complete words
+            words_count = words_count + 1;
+
+            // calculates count of non-space characters
+            chars_count = chars_count + words[word_num].chars().count() as u32;
+       
+            word_num = word_num + 1;
+            words.push("".to_string());
         } else {
-            current.push(c);
+            words[word_num].push(c);
         }
 
         i = i + 1;   
@@ -72,39 +79,44 @@ pub fn transform(input: &str, line_width: u32) -> String {
 
     if i == line_width + 1 {
 
-        if process_words(&mut words, &mut output, line_width, &mut chars_count) == false {
+        if process_words(&mut words, &mut words_count, &mut word_num, &mut output, line_width, &mut chars_count) == false {
             return "too long word".to_string();
         }
     }
-   
-    // if there is still something in current variable, finally process it
-    if current != "" {
-        words.push(current.clone());
-        chars_count = chars_count + current.chars().count() as u32;
-        output.push_str(&process_chunk(&words, line_width, chars_count));
+    
+    // if there are still some unprocessed words, finally process it
+    if words.len() > 0 && words[0] != "" {
+        chars_count = chars_count + words[word_num].chars().count() as u32;
+        output.push_str(&process_chunk(&words, words.len() as u32, line_width, chars_count));
     }
-
     output
 }
 
-fn process_words(words: &mut Vec<String>, output: &mut String, line_width: u32, chars_count: &mut u32) -> bool {
-    
+fn process_words(words: &mut Vec<String>, words_count: &mut u32, word_num: &mut usize, output: &mut String, line_width: u32, chars_count: &mut u32) -> bool {
+  
     // if words vector is empty then too long word encountered - return false
-    if words.len() == 0 {
+    if *words_count == 0 {
         return false;
     }
-    output.push_str(&process_chunk(words, line_width, *chars_count));
+    output.push_str(&process_chunk(words, *words_count, line_width, *chars_count));
     output.push('\n');
 
-    words.clear();
     *chars_count = 0;
+
+    // drain all complete words from the vector preserving last incomplete word, if exist
+    words.drain(0..*words_count as usize);
+    if words.len() == 0 {
+        words.push("".to_string());         
+    }
+
+    *words_count = 0;
+    *word_num = 0;
 
     true
 }
 
-fn process_chunk(words: &Vec<String>, line_width: u32, chars_count: u32) -> String {
+fn process_chunk(words: &Vec<String>, words_count: u32, line_width: u32, chars_count: u32) -> String {
     let mut chunk = String::new();
-    let words_count = words.len() as u32;
     let total_spaces = line_width - chars_count;
 
     if words_count == 1 {
@@ -126,6 +138,9 @@ fn process_chunk(words: &Vec<String>, line_width: u32, chars_count: u32) -> Stri
          // enumerate all words, pad each of them except the latest with word_spaces number of spaces
          // and additionally add one space to each word from left to right redusing rest_spaces by 1 until it become 0
          for (i, w) in words.iter().enumerate() {
+            if i >= words_count as usize {
+                break;
+            }
             chunk.push_str(w);
             if (i as u32) < words_count - 1 {
                let ws = word_spaces as usize;
